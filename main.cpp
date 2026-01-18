559a5e998ecc22dda6ed439d7eb3e20e3c02bc83
aaporitaranta <ritarantaaapo@gmail.com>
2026-01-18 23:52:26 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "GameNew.h" 
+
+using namespace std;
+
+int main()
+{
+    int maxNum = 20;
+    
+   
+    GameNew peliOlio(maxNum);
+    
+    
+    peliOlio.play();
+    
+ 
+
+    return 0;
+}
